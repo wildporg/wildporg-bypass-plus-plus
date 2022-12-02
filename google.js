1e94615d2aeee64e7cf4edcfa3b4f40e072399f3
wildporg <118760004+wildporg@users.noreply.github.com>
2022-12-02 13:30:01 +0000

    Added "to google" button and preferences option

--- google.js
+++ google.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Bypass++
 // @namespace    https://github.com/wildporg/wildporg-bypass-plus-plus/blob/main/google.js
-// @version      1.0.1
+// @version      1.1.0
 // @description  This script lets you bypass most website blockers... With google.
 // @author       wildporg (https://github.com/wildporg)
 // @match        *://*/*
 // @icon         http://github.com/favicon.ico
-// @grant        none
+// @grant        GM_setValue
+// @grant        GM_getValue
 // @noframes     true
 // ==/UserScript==
 
@@ -38,6 +39,32 @@
             };
         };
     } else {
+        if (GM_getValue("googleWindow")) {} else {
+            if (confirm('Do you want to have a "To Google" button at all times?')) {
+                GM_setValue("googleWindow", true);
+                window.alert("Preferences saved!");
+            } else {
+                GM_setValue("googleWindow", false);
+                window.alert("Preferences saved!");
+            };
+        };
+        if (GM_getValue("googleWindow") == true) {
+            var googleDiv = document.createElement("div");
+            googleDiv.style.cssText = "position: fixed; z-index: 10000; top: 0px; left: 0px; margin: 1vw; background: #18B; border-radius: 1em/1em;";
+            var googleButton = document.createElement("button");
+            googleButton.innerHTML = "Back to google (without a trace!)"
+            googleButton.style.cssText = "color: #FFFFFF; font-size: 1em; border: none; background: #00000000; padding: 0.75em; padding-right: 0px;";
+            googleButton.addEventListener("click", toGoogle);
+            var closeButton = document.createElement("button");
+            closeButton.innerHTML = "×";
+            closeButton.style.cssText = "color: #FFFFFF; font-size: 1em; border: none; background: #00000000; padding: 0.75em;";
+            closeButton.addEventListener("click", closeGoogleDiv);
+            googleDiv.append(closeButton)
+            googleDiv.prepend(googleButton)
+            document.body.prepend(googleDiv);
+            function toGoogle() {window.location.replace("https://www.google.com")};
+            function closeGoogleDiv() {googleDiv.remove();};
+        };
         if (document.getElementById("gt-nvframe")) {
             document.getElementById("gt-nvframe").remove();
             document.body.style.marginTop = "0px";
